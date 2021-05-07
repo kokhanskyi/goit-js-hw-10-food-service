8e1f4bbe5efdd3b452aa84170018479ea3b0dff4
kokhanskyi <79640742+kokhanskyi@users.noreply.github.com>
2021-05-07 17:08:56 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './styles.css';
+// console.log('Hi');
+import './js/test';
+
+// import print from './js/test';   default
+
+// console.log(print);
+
+// print('Not Hi');
+
+// import { text, printText } from './js/test';
+// console.log(text);
+// printText('J - S');
+
+import * as objectTT from './js/test';
+
+console.log(objectTT.text);
+
+objectTT.printText('uykjkjhkjhkjh');
+
+objectTT.data.test = 5;
+
+console.log(objectTT.data);
---
+++ src/js/test.js
@@ -0,0 +1,21 @@
+import menu from '../menu.json';
+
+// export const text = 'TEXT TEST';
+// export function printText(string) {
+//   console.log(string);
+// }
+
+const data = {};
+console.log(data);
+
+const text = 'TEXT TEST';
+function printText(string) {
+  console.log(string);
+}
+printText(text);
+
+console.log(menu);
+
+export { text, printText, data };
+
+// export default printText;     default
