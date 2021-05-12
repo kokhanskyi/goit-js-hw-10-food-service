6315ef9d9b27f520088bd876c656d3eb20af706b
kokhanskyi <79640742+kokhanskyi@users.noreply.github.com>
2021-05-12 17:28:32 +0300

    dz-10

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 import './styles.css';
-// console.log('Hi');
-import './js/test';
+import makeMenu from './templates/menu.hbs';
+import menuList from './menu.json';
+const menu__list = document.querySelector('.js-menu');
+const menuMarkup = makeMenu(menuList);
+menu__list.insertAdjacentHTML('beforeend', menuMarkup);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const themeSwitch = document.querySelector('.theme-switch__toggle');
 
-// import print from './js/test';   default
+themeSwitch.addEventListener('change', changeTheme);
+function changeTheme(event) {
+  if (event.target.checked) {
+    updateTheme(Theme.LIGHT, Theme.DARK);
+  } else {
+    updateTheme(Theme.DARK, Theme.LIGHT);
+  }
+}
+function updateTheme(oldTheme, newTheme) {
+  document.body.classList.add(newTheme);
+  document.body.classList.remove(oldTheme);
+  localStorage.setItem('pageTheme', newTheme);
+}
 
-// console.log(print);
+const curTeme = localStorage.getItem('pageTheme') || Theme.LIGHT;
+document.body.classList.add(curTeme);
+themeSwitch.checked = curTeme === Theme.DARK;
 
-// print('Not Hi');
-
-// import { text, printText } from './js/test';
-// console.log(text);
-// printText('J - S');
-
-import * as objectTT from './js/test';
-
-console.log(objectTT.text);
-
-objectTT.printText('uykjkjhkjhkjh');
-
-objectTT.data.test = 5;
-
-console.log(objectTT.data);
+// if (curTeme === Theme.DARK) {
+//   document.body.classList.add(Theme.DARK);
+//   themeSwitch.checked = true;
+// } else {
+//   document.body.classList.add(Theme.LIGHT);
+// }
